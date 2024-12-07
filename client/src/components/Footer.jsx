b7460d2154e425a49ca14a000bde101da0666a57
Amit Sisodiya <amitsisodiyajadaun@gmail.com>
2024-12-08 00:45:36 +0530

    kartify logo add

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -7,16 +7,17 @@ const Footer = () => {
   return (
     <footer className='border-t'>
         <div className='container mx-auto p-4 text-center flex flex-col lg:flex-row lg:justify-between gap-2'>
-            <p>© All Rights Reserved 2024.</p>
+            <p>© All Rights Reserved 2024</p>
+            <p>Designed and Develop by Amit Sisodiya</p>
 
             <div className='flex items-center gap-4 justify-center text-2xl'>
-                <a href='' className='hover:text-primary-100'>
+                <a href='' target='blank' className='hover:text-primary-100'>
                     <FaFacebook/>
                 </a>
-                <a href='' className='hover:text-primary-100'>
+                <a href='https://www.instagram.com/amit.asj.7/' target='blank' className='hover:text-primary-100'>
                     <FaInstagram/>
                 </a>
-                <a href='' className='hover:text-primary-100'>
+                <a href='https://www.linkedin.com/in/amit-sisodiya-184729213/' target='blank' className='hover:text-primary-100'>
                     <FaLinkedin/>
                 </a>
             </div>
